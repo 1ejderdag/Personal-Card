8420201aacb1de2338e76f2b13eacccf2d5877b9
Ejder Dag <ejdrdag@gmail.com>
2024-05-07 11:05:36 +0300

    Initial Commit

---
+++ EjderCard/EjderCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  EjderCardApp.swift
+//  EjderCard
+//
+//  Created by Ejder Dağ on 7.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct EjderCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
